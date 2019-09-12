1dc938ebb27b3757bfa5b9efc62df18e8887b2dc
Simon Rodriguez <kosua20@gmail.com>
2019-09-13 00:22:07 +0200

    Add build configuration.

---
+++ premake5.lua
@@ -0,0 +1,75 @@
+
+workspace("sr_webcam")
+
+-- Configurations
+	configurations({ "Release", "Debug"})
+	location("build")
+	targetdir ("build/%{prj.name}/%{cfg.longname}")
+	debugdir ("build/%{prj.name}/%{cfg.longname}")
+	architecture("x64")
+
+	-- Configuration specific settings.
+	filter("configurations:Release")
+		defines({ "NDEBUG" })
+		optimize("On")
+
+	filter("configurations:Debug")
+		defines({ "DEBUG" })
+		symbols("On")
+
+	filter({})
+	startproject("sr_webcam")
+
+project("sr_webcam")
+	kind("StaticLib")
+	systemversion("latest")
+	language("C")
+
+	filter("toolset:not msc*")
+		buildoptions({ "-Wall", "-Wextra" })
+	filter("toolset:msc*")
+		buildoptions({ "-W3"})
+	filter({})
+
+	-- common files
+	files({"include/sr_webcam.h", "src/sr_webcam_internal.h", "src/sr_webcam.c"})
+	
+	-- system build filters
+	filter("system:windows")
+		files({"src/sr_webcam_win.c"})
+
+	filter("system:macosx")
+		files({"src/sr_webcam_mac.m"})
+
+	filter({"system:linux"})
+		files({"src/sr_webcam_lin.c"})
+
+	-- visual studio filters
+	--filter("action:vs*")
+	--	defines({ "_CRT_SECURE_NO_WARNINGS" })    
+
+	filter({})
+
+include("examples/premake5.lua")
+
+-- Actions
+
+newaction {
+   trigger     = "clean",
+   description = "Clean the build directory",
+   execute     = function ()
+      print("Cleaning...")
+      os.rmdir("./build")
+      print("Done.")
+   end
+}
+
+newaction {
+   trigger     = "docs",
+   description = "Build the documentation using Doxygen",
+   execute     = function ()
+      print("Generating documentation...")
+      os.execute("doxygen"..ext.." docs/Doxyfile")
+      print("Done.")
+   end
+}
