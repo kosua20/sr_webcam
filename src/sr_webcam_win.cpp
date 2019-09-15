f538baadb6c5b08f2af39754f6fdcad43ce88eb0
Simon Rodriguez <kosua20@gmail.com>
2019-09-15 13:22:45 +0200

    Windows: cleanup setup.

--- src/sr_webcam_win.cpp
+++ src/sr_webcam_win.cpp
@@ -112,153 +112,296 @@ namespace {
 
 }
 
-
-int sr_webcam_setup(sr_webcam_device * device) {
-	MFContext & contex = MFContext::getContext();
-	_ComPtr<IMFSourceReaderCallback> readCallback;
-	_ComPtr<IMFSourceReader> videoFileSource;
-	LONGLONG frameStep;
+struct Format {
 	
+	unsigned int frameSize = 0;
+	UINT32 height = 0;
+	UINT32 width = 0;
+	int stride = 0; // stride is negative if image is bottom-up
+	unsigned int fixedSizeSamples = 0;
+	UINT32 frameRateNum = 0;
+	UINT32 frameRateDenom = 0;
+	unsigned int sampleSize = 0;
+	unsigned int interlaceMode = 0;
+	GUID type;
+	GUID subType;
+	double frameRate = 0.0;
+	LONGLONG frameStep = 0;
 	
-	if(device->deviceId < 0){
-		return 1;
+	Format(){
+		memset(&type, 0, sizeof(GUID));
+		memset(&subType, 0, sizeof(GUID));
 	}
 	
-	ComPtr<IMFAttributes> msAttr = NULL;
-	if(!(SUCCEEDED(MFCreateAttributes(&msAttr, 1)) &&
-		SUCCEEDED(msAttr->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID )))){
-		return 1;
-	}
-	IMFActivate **ppDevices = NULL;
-	UINT32 count;
-	bool opened = false;
-	// Enumerate devices.
-	if(SUCCEEDED(MFEnumDeviceSources(msAttr.Get(), &ppDevices, &count))){
-		if(count > 0){
-			for(int i = 0; i < (int)count; ++i){
-				if(!ppDevices[i]){
+	Format(IMFMediaType *pType){
+		memset(&type, 0, sizeof(GUID));
+		memset(&subType, 0, sizeof(GUID));
+		
+		UINT32 count = 0;
+		if(SUCCEEDED(pType->GetCount(&count)) && SUCCEEDED(pType->LockStore())) {
+			for (UINT32 i = 0; i < count; i++){
+				PROPVARIANT var;
+				PropVariantInit(&var);
+				GUID guid = { 0 };
+				if(!SUCCEEDED(pAttr->GetItemByIndex(i, &guid, &var))){
 					continue;
 				}
-				if(i == device->deviceId){
-					// Set source reader parameters
-					_ComPtr<IMFMediaSource> mSrc;
-					_ComPtr<IMFAttributes> srAttr;
-					
-					if(!SUCCEEDED( ppDevices[ind]->ActivateObject( __uuidof(IMFMediaSource), (void**)&mSrc ))){
-						continue;
+				// Extract the properties we need.
+				if(guid == MF_MT_DEFAULT_STRIDE && var.vt == VT_INT){
+					stride = var.intVal;
+				} else if(guid == MF_MT_FRAME_RATE && var.vt == VT_UI8){
+					Unpack2UINT32AsUINT64(var.uhVal.QuadPart, &frameRateNum, &frameRateDenom);
+					// Compute framerate.
+					if(frameRateDenom != 0){
+						frameRate = ((double)frameRateNum) / ((double)frameRateDenom);
 					}
-					if(!mSrc){
-						continue;
-					}
-					if(!SUCCEEDED(MFCreateAttributes(&srAttr, 10))){
-						continue;
-					}
-					if(!SUCCEEDED(srAttr->SetUINT32(MF_READWRITE_ENABLE_HARDWARE_TRANSFORMS, TRUE))){
-						continue;
-					}
-					if(!SUCCEEDED(srAttr->SetUINT32(MF_SOURCE_READER_DISABLE_DXVA, FALSE))){
-						continue;
-					}
-					if(!SUCCEEDED(srAttr->SetUINT32(MF_SOURCE_READER_ENABLE_VIDEO_PROCESSING, FALSE))){
-						continue;
-					}
-					if(!SUCCEEDED(srAttr->SetUINT32(MF_SOURCE_READER_ENABLE_ADVANCED_VIDEO_PROCESSING, TRUE))){
-						continue;
-					}
-					// Define callback.
-					//readCallback = ComPtr<IMFSourceReaderCallback>(new SourceReaderCB());
-					HRESULT hr = srAttr->SetUnknown(MF_SOURCE_READER_ASYNC_CALLBACK, (IMFSourceReaderCallback*)readCallback.Get());
-					if(FAILED(hr)){
-						readCallback.Release();
-						continue;
-					}
-					
-					if(SUCCEEDED(MFCreateSourceReaderFromMediaSource(mSrc.Get(), srAttr.Get(), &videoFileSource))){
-						opened = true;
-						// Look at the valid formats.
-						HRESULT hr = S_OK;
-						int dwStreamBest = -1;
-						//MediaType MTBest;
-						
-						DWORD dwMediaTypeTest = 0;
-						DWORD dwStreamTest = 0;
-						while(SUCCEEDED(hr)) {
-							_ComPtr<IMFMediaType> pType;
-							hr = videoFileSource->GetNativeMediaType(dwStreamTest, dwMediaTypeTest, &pType);
-							if(hr == MF_E_NO_MORE_TYPES) {
-								hr = S_OK;
-								++dwStreamTest;
-								dwMediaTypeTest = 0;
-								continue;
-							} else if (SUCCEEDED(hr)) {
-								MediaType MT(pType.Get());
-								if (MT.MF_MT_MAJOR_TYPE == MFMediaType_Video)
-								{
-									if (dwStreamBest < 0 ||
-										resolutionDiff(MT, width, height) < resolutionDiff(MTBest, width, height) ||
-										(resolutionDiff(MT, width, height) == resolutionDiff(MTBest, width, height) && MT.width > MTBest.width) ||
-										(resolutionDiff(MT, width, height) == resolutionDiff(MTBest, width, height) && MT.width == MTBest.width && MT.height > MTBest.height) ||
-										(MT.width == MTBest.width && MT.height == MTBest.height && (getFramerate(MT) > getFramerate(MTBest) && (prefFramerate == 0 || getFramerate(MT) <= prefFramerate)))
-										)
-									{
-										dwStreamBest = (int)dwStreamTest;
-										MTBest = MT;
-									}
-								}
-								++dwMediaTypeTest;
-							}
-						}
-						// Configure.
-						if (dwStreamBest >= 0)
-						{
-							GUID outSubtype = GUID_NULL;
-							UINT32 outStride = 0;
-							UINT32 outSize = 0;
-							// We do conversion if needed.
-							outSubtype =MFVideoFormat_RGB24; // HW accelerated mode support only RGB32 captureMode == MODE_HW ? MFVideoFormat_RGB32 :
-							outStride = 3 * MTBest.width; //(captureMode == MODE_HW ? 4 : 3)
-							outSize = outStride * MTBest.height;
-							
-							_ComPtr<IMFMediaType>  mediaTypeOut;
-							if (// Set the output media type.
-								SUCCEEDED(MFCreateMediaType(&mediaTypeOut)) &&
-								SUCCEEDED(mediaTypeOut->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video)) &&
-								SUCCEEDED(mediaTypeOut->SetGUID(MF_MT_SUBTYPE, convertToFormat ? outSubtype : MTBest.MF_MT_SUBTYPE)) &&
-								SUCCEEDED(mediaTypeOut->SetUINT32(MF_MT_INTERLACE_MODE, convertToFormat ? MFVideoInterlace_Progressive : MTBest.MF_MT_INTERLACE_MODE)) &&
-								SUCCEEDED(MFSetAttributeRatio(mediaTypeOut.Get(), MF_MT_PIXEL_ASPECT_RATIO, aspectRatioN, aspectRatioD)) &&
-								SUCCEEDED(MFSetAttributeSize(mediaTypeOut.Get(), MF_MT_FRAME_SIZE, MTBest.width, MTBest.height)) &&
-								SUCCEEDED(mediaTypeOut->SetUINT32(MF_MT_FIXED_SIZE_SAMPLES, convertToFormat ? 1 : MTBest.MF_MT_FIXED_SIZE_SAMPLES)) &&
-								SUCCEEDED(mediaTypeOut->SetUINT32(MF_MT_SAMPLE_SIZE, convertToFormat ? outSize : MTBest.MF_MT_SAMPLE_SIZE)) &&
-								SUCCEEDED(mediaTypeOut->SetUINT32(MF_MT_DEFAULT_STRIDE, convertToFormat ? outStride : MTBest.MF_MT_DEFAULT_STRIDE)))//Assume BGR24 input
-							{
-								if (SUCCEEDED(videoFileSource->SetStreamSelection((DWORD)MF_SOURCE_READER_ALL_STREAMS, false)) &&
-									SUCCEEDED(videoFileSource->SetStreamSelection((DWORD)dwStreamBest, true)) &&
-									SUCCEEDED(videoFileSource->SetCurrentMediaType((DWORD)dwStreamBest, NULL, mediaTypeOut.Get()))
-									)
-								{
-									dwStreamIndex = (DWORD)dwStreamBest;
-									nativeFormat = MTBest;
-									aspectN = aspectRatioN;
-									aspectD = aspectRatioD;
-									outputFormat = outFormat;
-									convertFormat = convertToFormat;
-									captureFormat = MediaType(mediaTypeOut.Get());
-									// Success !
-									double fps = getFramerate(nativeFormat); // MT.MF_MT_FRAME_RATE_DENOMINATOR != 0 ? ((double)MT.MF_MT_FRAME_RATE_NUMERATOR) / ((double)MT.MF_MT_FRAME_RATE_DENOMINATOR) : 0
-									frameStep = (LONGLONG)(fps > 0 ? 1e7 / fps : 0);
-								}
-								close();
-							}
-						}
-					}
+					frameStep = (LONGLONG)(frameRate > 0 ? (1e7 / frameRate) : 0);
+				} else if(guid == MF_MT_DEFAULT_STRIDE && var.vt == VT_UI4){
+					stride = (int)var.ulVal;
+				} else if(guid == MF_MT_FIXED_SIZE_SAMPLES && var.vt == VT_UI4){
+					fixedSizeSamples = var.ulVal;
+				} else if(guid == MF_MT_SAMPLE_SIZE && var.vt == VT_UI4){
+					sampleSize = var.ulVal;
+				} else if(guid == MF_MT_INTERLACE_MODE && var.vt == VT_UI4){
+					interlaceMode = var.ulVal;
+				} else if(guid == MF_MT_MAJOR_TYPE && var.vt == VT_CLSID){
+					type = *var.puuid;
+				} else if(guid == MF_MT_SUBTYPE && var.vt == VT_CLSID){
+					subType = *var.puuid;
+				} else if(guid == MF_MT_FRAME_SIZE && var.vt == VT_UI8) {
+					Unpack2UINT32AsUINT64(var.uhVal.QuadPart, &width, &height);
+					// Compute frame size.
+					frameSize = width * height;
 				}
-				ppDevices[i]->Release();
+				PropVariantClear(&var);
 			}
+			pType->UnlockStore();
 		}
 	}
-	CoTaskMemFree(ppDevices);
-	/* Update device data. */
-	return opened ? 0 : 1;
+};
+
+class VideoStreamMediaFoundation {
+public:
+	
+	VideoStreamMediaFoundation() : context(MFContext::getContext()) {
+		
+	}
+	
+	~VideoStreamMediaFoundation(){
+		
+	}
+	
+	bool setupWith(int id, int framerate, int w, int h){
+		ComPtr<IMFAttributes> msAttr = NULL;
+		if(!(SUCCEEDED(MFCreateAttributes(&msAttr, 1)) &&
+			 SUCCEEDED(msAttr->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID )))){
+			return false;
+		}
+		IMFActivate **ppDevices = NULL;
+		UINT32 count = 0;
+		bool opened = false;
+		// Enumerate devices.
+		if(!SUCCEEDED(MFEnumDeviceSources(msAttr.Get(), &ppDevices, &count)) || count == 0 || id < 0){
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		_id = min((int)count-1, id);
+		// Release all the others.
+		for(int i = 0; i < (int)count; ++i){
+			if(i == _id || !ppDevices[i]){
+				continue;
+			}
+			ppDevices[i]->Release();
+		}
+		// If the device is null, not available.
+		if(!ppDevices[_id]){
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		// Set source reader parameters
+		_ComPtr<IMFMediaSource> mSrc;
+		_ComPtr<IMFAttributes> srAttr;
+		
+		if(!SUCCEEDED(ppDevices[_id]->ActivateObject( __uuidof(IMFMediaSource), (void**)&mSrc )) || !mSrc){
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		if(!SUCCEEDED(MFCreateAttributes(&srAttr, 10))){
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		srAttr->SetUINT32(MF_READWRITE_ENABLE_HARDWARE_TRANSFORMS, TRUE);
+		srAttr->SetUINT32(MF_SOURCE_READER_DISABLE_DXVA, FALSE);
+		srAttr->SetUINT32(MF_SOURCE_READER_ENABLE_VIDEO_PROCESSING, FALSE);
+		srAttr->SetUINT32(MF_SOURCE_READER_ENABLE_ADVANCED_VIDEO_PROCESSING, TRUE);
+		
+		// Define callback.
+		readCallback = ComPtr<IMFSourceReaderCallback>(new SourceReaderCB());
+		HRESULT res = srAttr->SetUnknown(MF_SOURCE_READER_ASYNC_CALLBACK, (IMFSourceReaderCallback*)readCallback.Get());
+		if(FAILED(res)){
+			readCallback.Release();
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		if(!SUCCEEDED(MFCreateSourceReaderFromMediaSource(mSrc.Get(), srAttr.Get(), &videoFileSource))){
+			readCallback.Release();
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		// Look at the valid formats.
+		HRESULT hr = S_OK;
+		
+		int bestStream = -1;
+		Format bestFormat;
+		float bestFit = 1e9;
+		
+		DWORD streamId = 0;
+		DWORD typeId = 0;
+		
+		// Iterate over streams and media types.
+		while(SUCCEEDED(hr)) {
+			_ComPtr<IMFMediaType> pType;
+			hr = videoFileSource->GetNativeMediaType(streamId, typeId, &pType);
+			// If we reach the end of format types for this stream, move to the next.
+			if(hr == MF_E_NO_MORE_TYPES) {
+				hr = S_OK;
+				++streamId;
+				typeId = 0;
+				continue;
+			}
+			// If hr is neither no_more_types nor success, we reached the end of the streams list.
+			if(!SUCCEEDED(hr)) {
+				continue;
+			}
+			Format format(pType.Get());
+			// We only care about video types.
+			if(format.type != MFMediaType_Video) {
+				++typeId;
+				continue;
+			}
+			// Init with the first existing format.
+			if(dwStreamBest < 0){
+				const float dw = (float)(w - format.width);
+				const float dh = (float)(h - format.height);
+				bestFit = sqrtf(dw*dw+dh*dh);
+				bestStream = (int)streamId;
+				bestFormat = format;
+				++typeId;
+				continue;
+			}
+			
+			// If the current best already has the same size, replace it if the framerate is closer to the requested one.
+			if(format.width == bestFormat.width && format.height == bestFormat.height){
+				if(abs(fps - format.framerate) < abs(fps - bestFormat.framerate)){
+					bestStream = (int)streamId;
+					bestFormat = format;
+				}
+				++typeId;
+				continue;
+			}
+			// Else, replace the format if its size is closer to the required one.
+			const float dw = (float)(w - format.width);
+			const float dh = (float)(h - format.height);
+			const float fit = sqrtf(dw*dw+dh*dh);
+			if(fit < bestFit){
+				bestFit = fit;
+				bestStream = (int)streamId;
+				bestFormat = format;
+			}
+			++typeId;
+		}
+		
+		if(bestStream < 0){
+			readCallback.Release();
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		// We found the best available stream and format, configure.
+		GUID outSubtype = MFVideoFormat_RGB24; // Note from OpenCV: HW only supports MFVideoFormat_RGB32.
+		UINT32 outStride = 3 * MTBest.width;
+		UINT32 outSize = outStride * MTBest.height;
+		
+		_ComPtr<IMFMediaType> typeOut;
+		if(!SUCCEEDED(MFCreateMediaType(&typeOut))){
+			readCallback.Release();
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		typeOut->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video);
+		typeOut->SetGUID(MF_MT_SUBTYPE, outSubtype);
+		typeOut->SetUINT32(MF_MT_INTERLACE_MODE, MFVideoInterlace_Progressive);
+		MFSetAttributeRatio(typeOut.Get(), MF_MT_PIXEL_ASPECT_RATIO, 1, 1);
+		MFSetAttributeSize(typeOut.Get(), MF_MT_FRAME_SIZE, bestFormat.width, bestFormat.height);
+		typeOut->SetUINT32(MF_MT_FIXED_SIZE_SAMPLES, 1);
+		typeOut->SetUINT32(MF_MT_SAMPLE_SIZE, outSize);
+		typeOut->SetUINT32(MF_MT_DEFAULT_STRIDE, outStride);
+		
+		if(!SUCCEEDED(videoFileSource->SetStreamSelection ((DWORD)MF_SOURCE_READER_ALL_STREAMS, false)) ||
+		   !SUCCEEDED(videoFileSource->SetStreamSelection ((DWORD)bestStream, true)) ||
+		   !SUCCEEDED(videoFileSource->SetCurrentMediaType((DWORD)bestStream, NULL, typeOut.Get()))){
+			videoFileSource.Release();
+			readCallback.Release();
+			ppDevices[_id]->Release();
+			CoTaskMemFree(ppDevices);
+			return false;
+		}
+		
+		selectedStream = (DWORD)bestStream;
+		nativeFormat = bestFormat;
+		captureFormat = MediaType(typeOut.Get());
+		
+		ppDevices[_id]->Release();
+		CoTaskMemFree(ppDevices);
+		
+		return true;
+	}
+	
+	void start(){
+	}
+	
+	void stop(){
+		
+	}
+
+public:
+	
+	sr_webcam_device * _parent = NULL;
+	int _id = -1;
+	Format captureFormat;
+	
+private:
+	MFContext & context;
+	_ComPtr<IMFSourceReaderCallback> readCallback;
+	_ComPtr<IMFSourceReader> videoFileSource;
+	DWORD selectedStream;
+	Format nativeFormat;
+}
+
+int sr_webcam_open(sr_webcam_device * device) {
+	VideoStreamMediaFoundation * stream = new VideoStreamMediaFoundation();
+	stream->_parent = device;
+	bool res = stream->setupWith(device->deviceId, device->framerate, device->width, device->height);
+	if(!res){
+		return -1;
+	}
+	device->stream = stream;
+	device->width = stream->captureFormat.width;
+	device->height = stream->captureFormat.height;
+	device->framerate = stream->captureFormat.frameRate;
+	device->deviceId = stream->_id;
+	return 0;
 }
 
 
@@ -269,7 +412,14 @@ void sr_webcam_start(sr_webcam_device * device) {
 
 void sr_webcam_stop(sr_webcam_device * device) {
 	if (device->stream) {
-		
+		/*if (is(Open))
+		 {
+		 isOpen = false;
+		 videoSample.Release();
+		 videoFileSource.Release();
+		 camid = -1;
+		 }
+		 readCallback.Release();*/
 	}
 }
 
